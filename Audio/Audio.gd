b35d9cfdffceae86337da509c102a91aa9e8ea9b
williancozzi <williancozzi@gmail.com>
2020-03-22 19:29:14 -0300

    colocando o audio em loop

--- Audio/Audio.gd
+++ Audio/Audio.gd
@@ -8,6 +8,9 @@ func change_music():
 			load_stream = load("res://Audio/Test_Level_music.wav")
 		"Menu":
 			load_stream = load("res://Audio/Menu_Music.wav")
-#
-#	$Music_Level.stream = load_stream
-#	$Music_Level.play()
+
+	$Music_Level.stream = load_stream
+	$Music_Level.play()
+	
+func _on_Music_Level_finished():
+	$Music_Level.play()
