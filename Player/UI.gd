499c657b87f06344e52959a30371995926212d6e
BodeNoites <vitordiasalves@gmail.com>
2020-03-30 12:59:45 -0300

    Dialogue feature bug corrected

--- Player/UI.gd
+++ Player/UI.gd
@@ -19,15 +19,23 @@ func _input(event):
 			$Menu_Options.show()
 
 func show_text(text:String):
-	$Messages/Panel/Text_Area.text = ""
+	print(text)
+	$Messages/Panel/Text_Area.visible_characters = 0
+	$Messages/Panel/Text_Area.text = text
 	$Messages/Panel.show()
-	
-	for character in text:
-		yield(get_tree().create_timer(0.09), "timeout")
-		$Messages/Panel/Text_Area.text += character
+	$Messages/Timer.start(0.1)
+
 
 func hide_text():
 	$Messages/Panel.hide()
 
 func restart_level():
 	get_tree().reload_current_scene()
+
+
+func _on_Timer_timeout():
+	print("accabou")
+	$Messages/Panel/Text_Area.visible_characters += 1
+	if $Messages/Panel/Text_Area.visible_characters != $Messages/Panel/Text_Area.text.length():
+		$Messages/Timer.start(0.1)
+	pass # Replace with function body.
