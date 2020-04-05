2964f2a35b922268b0aee939211980e114f9a695
DrZanuff <ricardo.machado.nwi@gmail.com>
2020-04-05 03:48:57 -0300

    Added Enemy Rat and NPC RatLord
    -Added a Fireball Animation to Fire Spell
    -Added a Enemy Rat
    -Added RatLord Animation

--- Colectables/Fire_Shard/Fire_Ball.gd
+++ Colectables/Fire_Shard/Fire_Ball.gd
@@ -5,6 +5,8 @@ var speed = 550
 
 func _physics_process(delta):
 	global_position.x += direction.x*speed*delta
+	
+	$AnimatedSprite.flip_h = true if direction.x <= 0 else false
 
 func _on_Spell_area_entered(area):
 	if area.is_in_group("Vine"):
@@ -12,4 +14,6 @@ func _on_Spell_area_entered(area):
 		queue_free()
 
 func _on_Spell_body_entered(body):
+	if body.has_method("apply_damage"):
+		body.apply_damage(1)
 	queue_free()
