6b4863da769c968fbdab63aa374736e3a58b44b2
Ricardo <ricardo.machado.nwi@gmail.com>
2020-04-08 15:44:43 -0300

    Added RustyNail hood in the Level.
    - Added some Crucial NPCs for Liberty Quest
    - Started Liberty Quest

---
+++ Levels/Fallshade_Level.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var quest_talk_to_arnold = false
+
+func _on_Moses_body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		quest_talk_to_arnold = true
+		$NPCs/RustyNail/Arnold.speech = "Arnold: Hum… Então Moses te enviou? Certo, eis o plano: Cavamos um túnel até o subterrâneo da Torre Branca. Lá achamos o Cristal da Lua que energiza metade dos Guardiões de Goven. Só que está protegido por armadilhas. Já perdemos dois homens neste buraco! Se você é tão bom quando dizem, você deve atravessar as armadilhas antes de anoitecer e destruir o Cristal. Assim que destruir, nós vamos saber que você conseguiu. Fuja pelo Portão da Lua, ao Leste e nos encontre fora da cidade."
+
+func _on_Arnold_body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		if quest_talk_to_arnold == true:
+			$Walls/FakeWall/AnimationPlayer.play("Fade")
+			$Walls/FakeWall.open_passage()
--- NPC/GenericNPC/GenericNPC.gd
+++ NPC/GenericNPC/GenericNPC.gd
@@ -5,6 +5,8 @@ export (Texture) var sprite setget set_texture
 
 export (String, MULTILINE)var speech = ""
 
+export (bool) var mirror setget set_mirror
+
 func _on_GenericNPC_body_entered(body):
 	if body.is_in_group("Player"):
 		owner.get_node("UI").show_text(speech)
@@ -16,3 +18,7 @@ func _on_GenericNPC_body_exited(body):
 func set_texture(tex):
 	sprite = tex
 	$Sprite.texture = tex
+	
+func set_mirror(dir):
+	mirror = dir
+	$Sprite.flip_h = dir
